f9686c040f7c411e504c6e8f85f374d5e0d64e80
Gus Sánchez <goosethedev@proton.me>
2024-12-04 13:37:59 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
-use std::{io::Write, net::TcpListener};
+use std::io::Cursor;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+use anyhow::Result;
+use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let buf = [0, 0, 0, 0, 0, 0, 0, 7];
-                stream.write_all(&buf).expect("Error sending response");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:9092").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            println!("accepted new connection");
+            handle_connection(stream).await.expect("connection error");
+        });
     }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    // Read message_size
+    let mut len = [0; 4];
+    stream.read_exact(&mut len).await?;
+    let len = i32::from_be_bytes(len);
+
+    // Read message content
+    let mut msg = vec![0; len as usize];
+    stream.read_exact(&mut msg).await?;
+
+    // Use a buffered cursor to read parts of the message
+    let mut msg = Cursor::new(msg);
+    msg.read_i16().await?; // request api key
+    msg.read_i16().await?; // request api version
+    let correlation_id = msg.read_i32().await?;
+
+    stream.write_all(&[0; 4]).await?;
+    stream.write_all(&correlation_id.to_be_bytes()).await?;
+
+    Ok(())
 }
